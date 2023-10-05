b9f9b37f7fad2768815ee25b45912ddc7534488a
khanh260802 <casiofx580@gmail.com>
2023-10-05 13:56:17 +0700

    lab 4

---
+++ STT_01NguyenKhanhAn_Tuan04/lab4/GlobalStyles.js
@@ -0,0 +1,31 @@
+/* fonts */
+export const FontFamily = {
+    robotoBold: "Roboto-Bold",
+    robotoRegular: "Roboto-Regular",
+  };
+  /* font sizes */
+  export const FontSize = {
+    size_xl: 20,
+    size_mini: 15,
+    size_6xl: 25,
+    size_lg: 18,
+    size_sm: 14,
+  };
+  /* Colors */
+  export const Color = {
+    colorWhite: "#fff",
+    colorBlack: "#000",
+    colorGold: "#e3c000",
+    colorBlue: "#0e18f5",
+    colorSilver_100: "#c4c4c4",
+    colorSilver_200: "rgba(196, 196, 196, 0)",
+    colorSilver_300: "rgba(196, 196, 196, 0.3)",
+    colorWhitesmoke: "#f2f2f2",
+    colorMediumseagreen: "rgba(49, 170, 82, 0.19)",
+    colorCrimson: "#e53935",
+  };
+  /* border radiuses */
+  export const Border = {
+    br_3xs: 10,
+    br_11xs: 2,
+  };
---
+++ STT_01NguyenKhanhAn_Tuan04/lab4/screens/TongHaiSo.js
@@ -0,0 +1,85 @@
+import {
+    Text,
+    View,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useState } from "react";
+
+const TongHaiSo = () => {
+    const [inputValue1, setInputValue1] = useState("");
+    const [inputValue2, setInputValue2] = useState("");
+    const [sum, setSum] = useState("");
+    const MyButton = ({ onPress, title }) => {
+        return (
+            <TouchableOpacity style={styles.button} onPress={onPress}>
+                <Text style={styles.buttonText}>{title}</Text>
+            </TouchableOpacity>
+        );
+    };
+    return (
+        <View style={styles.container}>
+            <View style={styles.inputContaier}>
+                <Text> Enter first value: </Text>
+                <TextInput
+                    style={styles.input}
+                    value={inputValue1}
+                    onChangeText={(text) => setInputValue1(text)}
+                />
+            </View>
+
+            <View style={styles.inputContaier}>
+                <Text> Enter second value: </Text>
+                <TextInput
+                    style={styles.input}
+                    value={inputValue2}
+                    onChangeText={(text) => setInputValue2(text)}
+                />
+            </View>
+
+            <View style={styles.inputContaier}>
+                <Text> result: </Text>
+                <Text style={styles.input}> {sum} </Text>
+            </View>
+
+            <MyButton
+                onPress={() =>
+                    setSum(parseInt(inputValue1) + parseInt(inputValue2))
+                }
+                title={"Tong hai so"}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: "gray",
+        padding: 10,
+        margin: 10,
+        width: "80%",
+    },
+
+    inputContaier: {},
+
+    button: {
+        backgroundColor: "blue",
+        padding: 10,
+        borderRadius: 5,
+        marginTop: 10,
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+});
+export default TongHaiSo;
